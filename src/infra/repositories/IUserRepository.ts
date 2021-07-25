2836806bfd66d271da1a91cb764c026743ddc41c
Fábio dos Santos <fah_ds@live.com>
2021-07-25 14:58:47 -0300

    feat: criado repositorio de usuário na camada de infra

---
+++ src/infra/repositories/IUserRepository.ts
@@ -0,0 +1,7 @@
+import { User } from '../../domain/entity/User';
+import { createUserDTO } from '../../domain/useCases/ICreateUser';
+
+export interface IUserRepository {
+  save(user: createUserDTO): Promise<User>;
+  getAllUsers(): Promise<User[]>;
+}
