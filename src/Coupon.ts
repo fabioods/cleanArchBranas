958b08dd7293b2bda4a5a03e85995c868eeac454
Fábio dos Santos <fah_ds@live.com>
2021-08-02 11:33:11 -0300

    feat: criada entidade para Cupom de desconto

---
+++ src/Coupon.ts
@@ -0,0 +1,10 @@
+export class Coupon {
+  code: string;
+
+  percentage: number;
+
+  constructor(code: string, percentage: number) {
+    this.code = code;
+    this.percentage = percentage;
+  }
+}
