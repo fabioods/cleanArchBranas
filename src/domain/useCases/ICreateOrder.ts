8a7ec9b5b7e223c913e043f306836be16f86ad87
Fábio dos Santos <fah_ds@live.com>
2021-08-03 15:42:26 -0300

    feat: adicionado ao ICreateOrder os campos de altura, peso, espessura e peso, alem de um dto para informar a origem e destino do pedido

--- src/domain/useCases/ICreateOrder.ts
+++ src/domain/useCases/ICreateOrder.ts
@@ -4,12 +4,22 @@ export type ICreateOrdemItemDTO = {
   description: string;
   quantity: number;
   price: number;
+  height: number;
+  width: number;
+  thickness: number;
+  weight: number;
+};
+
+export type ShippingDTO = {
+  source: string;
+  destination: string;
 };
 
 export type ICreateOrderDTO = {
   user_id: string;
   items: ICreateOrdemItemDTO[];
   coupon: string;
+  shipping?: ShippingDTO;
 };
 
 export interface ICreateOrder {
