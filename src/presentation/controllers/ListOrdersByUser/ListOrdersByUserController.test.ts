d6c51e767333c76fb4d7f9cc129d24cd9baf2bff
Fábio dos Santos <fah_ds@live.com>
2021-08-03 14:25:29 -0300

    refactor: listagem de pedidos do usuário de acordo com as novas adaptações implementadas

--- src/presentation/controllers/ListOrdersByUser/ListOrdersByUserController.test.ts
+++ src/presentation/controllers/ListOrdersByUser/ListOrdersByUserController.test.ts
@@ -1,31 +1,63 @@
 import { CreateOrderUseCase } from '../../../data/useCases/CreateOrderUseCase';
+import { CreateUserUseCase } from '../../../data/useCases/CreateUserUseCase';
 import { ListOrderByUserUseCase } from '../../../data/useCases/ListOrderByUserUseCase';
 import { ICreateOrderDTO } from '../../../domain/useCases/ICreateOrder';
+import { MemoryCouponRepository } from '../../../infra/repositories/implementation/MemoryCouponRepository';
 import { MemoryOrderRepository } from '../../../infra/repositories/implementation/MemoryOrderRepository';
+import { MemoryUserRepository } from '../../../infra/repositories/implementation/MemoryUserRepository';
+import { CPFValidatorAdapter } from '../../../utils/cpfValidatorAdapter';
 import { CreateOrderController } from '../createOrder/CreateOrderController';
+import { CreateUserController } from '../createUser/CreateUserController';
 import { ListOrderByUserController } from './ListOrdersByUserController';
 
 describe('List orders by user', () => {
   const makeSUT = () => {
+    const userRepository = new MemoryUserRepository();
+    const cpfValidatorAdapter = new CPFValidatorAdapter();
+    const createUserUseCase = new CreateUserUseCase(userRepository);
+    const createUserController = new CreateUserController(
+      cpfValidatorAdapter,
+      createUserUseCase
+    );
+
+    const couponRepository = new MemoryCouponRepository();
+
     const orderRepository = new MemoryOrderRepository();
-    const createOrderUserCase = new CreateOrderUseCase(orderRepository);
-    const listOrdersByUserUseCase = new ListOrderByUserUseCase(orderRepository);
+    const createOrderUserCase = new CreateOrderUseCase(
+      orderRepository,
+      couponRepository,
+      userRepository
+    );
     const createOrderController = new CreateOrderController(
       createOrderUserCase
     );
+
+    const listOrdersByUserUseCase = new ListOrderByUserUseCase(orderRepository);
     const listOrdersByUserController = new ListOrderByUserController(
       listOrdersByUserUseCase
     );
 
-    return { createOrderController, listOrdersByUserController };
+    return {
+      createOrderController,
+      listOrdersByUserController,
+      createUserController,
+    };
   };
 
   it('should be able to return the number of orders by user', async () => {
-    const { createOrderController, listOrdersByUserController } = makeSUT();
-    const order1 = {
-      user_id: 'XXXX',
-      discount: 0,
-      total: 10,
+    const {
+      createOrderController,
+      listOrdersByUserController,
+      createUserController,
+    } = makeSUT();
+
+    const createUser = await createUserController.handle({
+      body: { name: 'Fábio', cpf: '529.982.247-25' },
+    });
+
+    const order = {
+      user_id: createUser.body.user.id,
+      coupon: 'x',
       items: [
         {
           description: 'Coca-cola',
@@ -34,48 +66,21 @@ describe('List orders by user', () => {
         },
       ],
     } as ICreateOrderDTO;
-    const order2 = {
-      user_id: 'XXXX',
-      discount: 0,
-      total: 10,
-      items: [
-        {
-          description: 'Coca-cola',
-          quantity: 1,
-          price: 10,
-        },
-      ],
-    } as ICreateOrderDTO;
-    const order3 = {
-      user_id: 'YYYY',
-      discount: 0,
-      total: 10,
-      items: [
-        {
-          description: 'Coca-cola',
-          quantity: 1,
-          price: 10,
-        },
-      ],
-    } as ICreateOrderDTO;
+
     await createOrderController.handle({
       body: {
-        order: order1,
+        order,
       },
     });
     await createOrderController.handle({
       body: {
-        order: order2,
-      },
-    });
-    await createOrderController.handle({
-      body: {
-        order: order3,
+        order,
       },
     });
     const httpResponse = await listOrdersByUserController.handle({
-      body: { user_id: 'XXXX' },
+      body: { user_id: createUser.body.user.id },
     });
+
     expect(httpResponse.body.length).toBe(2);
   });
 });
