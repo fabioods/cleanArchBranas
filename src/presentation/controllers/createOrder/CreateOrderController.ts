00454d739d40814304736b354e73ce09511fae58
Fábio dos Santos <fah_ds@live.com>
2021-08-03 14:23:06 -0300

    refactor: refatorada forma de declarar variavel do usecase no controller de createOrder

--- src/presentation/controllers/createOrder/CreateOrderController.ts
+++ src/presentation/controllers/createOrder/CreateOrderController.ts
@@ -2,11 +2,7 @@ import { ICreateOrder } from '../../../domain/useCases/ICreateOrder';
 import { HttpRequest, HttpResponse, IController } from '../../protocols';
 
 export class CreateOrderController implements IController {
-  private createOrderUseCase: ICreateOrder;
-
-  constructor(createOrderUseCase: ICreateOrder) {
-    this.createOrderUseCase = createOrderUseCase;
-  }
+  constructor(private createOrderUseCase: ICreateOrder) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest;
