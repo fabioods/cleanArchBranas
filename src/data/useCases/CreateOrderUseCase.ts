618f687c816bdac9a3bb6f0f2fde04cced64a331
Fábio dos Santos <fah_ds@live.com>
2021-08-03 14:21:16 -0300

    refactor: refatorada estrutura de criação de pedido

--- src/data/useCases/CreateOrderUseCase.ts
+++ src/data/useCases/CreateOrderUseCase.ts
@@ -3,20 +3,37 @@ import {
   ICreateOrder,
   ICreateOrderDTO,
 } from '../../domain/useCases/ICreateOrder';
+import { ICouponRepository } from '../../infra/repositories/ICouponRepository';
 import { IOrderRepository } from '../../infra/repositories/IOrderRepository';
+import { IUserRepository } from '../../infra/repositories/IUserRepository';
 
 export class CreateOrderUseCase implements ICreateOrder {
-  private orderRepository: IOrderRepository;
-
-  constructor(orderRepository: IOrderRepository) {
-    this.orderRepository = orderRepository;
-  }
+  constructor(
+    private orderRepository: IOrderRepository,
+    private couponRepository: ICouponRepository,
+    private userRepository: IUserRepository
+  ) {}
 
   async createOrder(order: ICreateOrderDTO): Promise<Order> {
     if (!order.user_id) throw new Error('Missing user_id');
+    const user = await this.userRepository.findById(order.user_id);
+    if (!user) throw new Error('User not found');
+
     if (order.items.length === 0)
       throw new Error('Order must have at least one item');
-    const newOrder = this.orderRepository.save(order);
-    return newOrder;
+
+    const coupon = await this.couponRepository.findByCoupon(order.coupon);
+    if (coupon && !coupon.isValid()) throw new Error('Invalid coupon');
+
+    const newOrder = new Order(user);
+    if (coupon) newOrder.addCoupon(coupon);
+
+    for (let index = 0; index < order.items.length; index++) {
+      const element = order.items[index];
+      newOrder.addItem(element.description, element.quantity, element.price);
+    }
+
+    const orderSaved = await this.orderRepository.save(newOrder);
+    return orderSaved;
   }
 }
