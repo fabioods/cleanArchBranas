d8a0fde6372bbf1be6c52242972a34231dc525c1
Fábio dos Santos <fah_ds@live.com>
2021-08-03 14:04:16 -0300

    feat: adicionado ao item do pedido um construtor e um método para calcular o total do item

--- src/domain/entity/OrderItem.ts
+++ src/domain/entity/OrderItem.ts
@@ -1,7 +1,29 @@
-export type OrderItem = {
+export class OrderItem {
   id: string;
+
   description: string;
+
   quantity: number;
+
   price: number;
+
   order_id: string;
-};
+
+  getTotal(): number {
+    return this.quantity * this.price;
+  }
+
+  constructor(
+    description: string,
+    quantity: number,
+    price: number,
+    order_id?: string,
+    id?: string
+  ) {
+    this.description = description;
+    this.quantity = quantity;
+    this.price = price;
+    if (order_id) this.order_id = order_id;
+    if (id) this.id = id;
+  }
+}
