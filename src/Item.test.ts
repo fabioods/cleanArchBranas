3520bfdac9172c3aa326174f3a577ef41ee85bdb
Fábio dos Santos <fah_ds@live.com>
2021-08-10 10:26:07 -0300

    test: entidade para item

---
+++ src/Item.test.ts
@@ -0,0 +1,15 @@
+import { Item } from './Item';
+
+describe('Criação do objeto de item', () => {
+  it('deve calcular o volume de um item', () => {
+    const item = new Item('1', 'Amplificador', 5000, 50, 50, 50, 22);
+    const volume = item.getVolume();
+    expect(volume).toBe(0.125);
+  });
+
+  it('deve calcular a densidade de um item', () => {
+    const item = new Item('1', 'Amplificador', 5000, 50, 50, 50, 22);
+    const density = item.getDensity();
+    expect(density).toBe(176);
+  });
+});
