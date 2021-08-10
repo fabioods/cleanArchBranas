dc3e1ecc3212f08497046d0d69b9f58e5195447f
Fábio dos Santos <fah_ds@live.com>
2021-08-10 10:23:09 -0300

    feat: cadastro de cupom

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -3,8 +3,16 @@ export class Coupon {
 
   percentage: number;
 
-  constructor(code: string, percentage: number) {
+  expireDate: Date;
+
+  constructor(code: string, percentage: number, expireDate: Date) {
     this.code = code;
     this.percentage = percentage;
+    this.expireDate = expireDate;
+  }
+
+  isExpired(): boolean {
+    const today = new Date();
+    return this.expireDate.getTime() < today.getTime();
   }
 }
