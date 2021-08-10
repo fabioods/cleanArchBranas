15382aa5e067b94d53ab1d21f3ef40e608a0115f
Fábio dos Santos <fah_ds@live.com>
2021-08-10 10:24:20 -0300

    test: adicionado data de expiração do cupom no placeorder

--- src/PlaceOrder.test.ts
+++ src/PlaceOrder.test.ts
@@ -18,4 +18,22 @@ describe('Realizar pedido', () => {
     const output = placeOrder.execute(input);
     expect(output.total).toBe(5672);
   });
+
+  it('Deve fazer um pedido com cupom de desconto expirado', () => {
+    const valid_cpf = '778.278.412-36';
+
+    const input = {
+      cpf: valid_cpf,
+      items: [
+        { description: 'Guitarra', quantity: 1000, price: 2 },
+        { description: 'Amplificador', quantity: 5000, price: 1 },
+        { description: 'Cabo', quantity: 30, price: 3 },
+      ],
+      coupon: 'VALE20_EXPIRED',
+    };
+
+    const placeOrder = new PlaceOrder();
+    const output = placeOrder.execute(input);
+    expect(output.total).toBe(7090);
+  });
 });
