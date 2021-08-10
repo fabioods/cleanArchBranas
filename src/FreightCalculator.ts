f6253bb10ba97a16826868219ec5a37363dba6d6
Fábio dos Santos <fah_ds@live.com>
2021-08-10 12:36:30 -0300

    refactor: adicionada condição para quando o preço for menor q 10, o valor é 10

--- src/FreightCalculator.ts
+++ src/FreightCalculator.ts
@@ -2,6 +2,7 @@ import { Item } from './Item';
 
 export class FreightCalculator {
   static calculate(item: Item, distance: number): number {
-    return distance * item.getVolume() * (item.getDensity() / 100);
+    const price = distance * item.getVolume() * (item.getDensity() / 100);
+    return price > 10 ? price : 10;
   }
 }
