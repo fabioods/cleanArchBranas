793fc384d284e4916a90c1fb5680993da897117b
Fábio dos Santos <fah_ds@live.com>
2021-08-13 19:05:47 -0300

    feat: adicionado metodo para buscar usuario por id

--- src/domain/repository/UserRepository.ts
+++ src/domain/repository/UserRepository.ts
@@ -2,4 +2,5 @@ import { User } from '../entity/User';
 
 export interface UserRepository {
   save(user: User): Promise<User>;
+  getById(id: string): Promise<User | undefined>;
 }
--- src/infra/repository/memory/UserRepositoryMemory.ts
+++ src/infra/repository/memory/UserRepositoryMemory.ts
@@ -8,6 +8,10 @@ export class UserRepositoryMemory implements UserRepository {
     this.user = [];
   }
 
+  async getById(id: string): Promise<User | undefined> {
+    return this.user.find(u => u.id === id);
+  }
+
   async save(user: User): Promise<User> {
     Object.assign(user, { id: this.user.length + 1 });
     this.user.push(user);
