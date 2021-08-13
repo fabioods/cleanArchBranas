2aff349ba9e7522fda84f19c82d4c9eaff166237
Fábio dos Santos <fah_ds@live.com>
2021-08-13 19:05:06 -0300

    test: entidade de order recebe o user id ao inves do user

--- src/domain/entity/Order.test.ts
+++ src/domain/entity/Order.test.ts
@@ -1,11 +1,9 @@
 import { Coupon } from './Coupon';
 import { Order } from './Order';
-import { User } from './User';
 
 describe('Create Order', () => {
   it('should create a new order with three itens that sum 11', () => {
-    const user = new User('any_name', 'any_cpf');
-    const order = new Order(user);
+    const order = new Order('any_user_id');
     order.addItem('1', 1, 1);
     order.addItem('2', 2, 2);
     order.addItem('3', 2, 3);
@@ -13,9 +11,8 @@ describe('Create Order', () => {
   });
 
   it('should create a new order a coupon for discount', () => {
-    const user = new User('any_name', 'any_cpf');
     const coupon = new Coupon('Coupon X', 10, new Date());
-    const order = new Order(user);
+    const order = new Order('any_user_id');
     order.addItem('1', 2, 2);
     order.addItem('2', 2, 3);
     order.addCoupon(coupon);
