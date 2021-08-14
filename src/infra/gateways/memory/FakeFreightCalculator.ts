2e1390ee35a7568f5b266b241270ec2076486654
Fábio dos Santos <fah_ds@live.com>
2021-08-14 13:53:53 -0300

    feat: valor minimo do frete sendo 10

--- src/infra/gateways/memory/FakeFreightCalculator.ts
+++ src/infra/gateways/memory/FakeFreightCalculator.ts
@@ -5,7 +5,7 @@ import { FreightCalculator } from '../../../providers/FreightCalculator';
 export class FakeFreightCalculator implements FreightCalculator {
   async calculatePrice(distance: number, item: Item): Promise<number> {
     const price = distance * item.getVolume() * (item.getDensity() / 100);
-    return price;
+    return price < 10 ? 10 : price;
   }
 
   async calculateDistance(
