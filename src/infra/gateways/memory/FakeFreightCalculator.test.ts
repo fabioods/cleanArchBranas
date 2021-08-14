f23046413dd9f280078c56af5ff42ecb7f2aaad5
Fábio dos Santos <fah_ds@live.com>
2021-08-14 12:58:46 -0300

    test: calculo de distancia e preço de frete para um item

--- src/infra/gateways/memory/FakeFreightCalculator.test.ts
+++ src/infra/gateways/memory/FakeFreightCalculator.test.ts
@@ -1,3 +1,4 @@
+import { Item } from '../../../domain/entity/Item';
 import { FakeFreightCalculator } from './FakeFreightCalculator';
 
 describe('Calculate Freight', () => {
@@ -5,10 +6,18 @@ describe('Calculate Freight', () => {
     const zipCodeOrigin = 'any_origin';
     const zipCodeDestination = 'any_destination';
     const freightCalculator = new FakeFreightCalculator();
-    const distance = await freightCalculator.calculate(
+    const distance = await freightCalculator.calculateDistance(
       zipCodeOrigin,
       zipCodeDestination
     );
     expect(distance).toBe(1000);
+  });
+
+  it('should calculate the price for an item', async () => {
+    const distance = 1000;
+    const item = new Item('any_description', 10, 10, 10, 10, 1000);
+    const freightCalculator = new FakeFreightCalculator();
+    const price = await freightCalculator.calculatePrice(distance, item);
+    expect(price).toBe(100);
   });
 });
