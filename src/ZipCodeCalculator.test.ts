55c8b76fac701e5322644752dd0bd5f338d14f96
Fábio dos Santos <fah_ds@live.com>
2021-08-16 13:37:26 -0300

    feat: criado repositorio e implementação em memória para coupon, item, order e zipcode

--- src/ZipCodeCalculator.test.ts
+++ src/ZipCodeCalculator.test.ts
@@ -1,8 +1,8 @@
-import { ZipCodeCalculatorMemory } from './ZipCodeCalculatorMemory';
+import { ZipcodeCalculatorAPIMemory } from './ZipcodeCalculatorAPIMemory';
 
 describe('ZipCode calculator', () => {
   it('deve calcular a distancia entre 2 ceps', () => {
-    const zipCodeCalculator = new ZipCodeCalculatorMemory();
+    const zipCodeCalculator = new ZipcodeCalculatorAPIMemory();
     const distance = zipCodeCalculator.calculate('11.111-11', '11.111-20');
     expect(distance).toBe(1000);
   });
--- src/ZipcodeCalculatorAPIMemory.ts
+++ src/ZipcodeCalculatorAPIMemory.ts
@@ -1,6 +1,6 @@
 import { ZipCodeCalculatorAPI } from './ZipCodeCalculatorAPI';
 
-export class ZipCodeCalculatorMemory implements ZipCodeCalculatorAPI {
+export class ZipcodeCalculatorAPIMemory implements ZipCodeCalculatorAPI {
   calculate(zipCodeA: string, zipCodeB: string): number {
     return 1000;
   }
