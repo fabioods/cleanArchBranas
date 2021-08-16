3f193a2fd6482025a3ece87a1b813305ced84b50
Fábio dos Santos <fah_ds@live.com>
2021-08-16 13:38:07 -0300

    refactor: removido implementações internas no place order e feita inversão de dependência

--- src/PlaceOrder.ts
+++ src/PlaceOrder.ts
@@ -1,56 +1,45 @@
 import { Coupon } from './Coupon';
+import CouponRepository from './CouponRepository';
 import { FreightCalculator } from './FreightCalculator';
-import { Item } from './Item';
+import ItemRepository from './ItemRepository';
 import { Order } from './Order';
+import OrderRepository from './OrderRepository';
 import { PlaceOrderInput } from './PlaceOrderInput';
 import { PlaceOrderOutput } from './PlaceOrderOutput';
 import { ZipCodeCalculatorAPI } from './ZipCodeCalculatorAPI';
-import { ZipCodeCalculatorMemory } from './ZipCodeCalculatorMemory';
 
 export class PlaceOrder {
-  coupons: Coupon[];
+  constructor(
+    private itemRepository: ItemRepository,
+    private couponRepository: CouponRepository,
+    private orderRepository: OrderRepository,
+    private zipcodeCalculator: ZipCodeCalculatorAPI
+  ) {}
 
-  orders: Order[];
-
-  items: Item[];
-
-  zipCodeCalculator: ZipCodeCalculatorAPI;
-
-  constructor() {
-    this.coupons = [
-      new Coupon('VALE20', 20, new Date('2021-10-10')),
-      new Coupon('VALE20_EXPIRED', 20, new Date('2020-10-10')),
-    ];
-    this.orders = [];
-    this.items = [
-      new Item('1', 'Guitarra', 1000, 100, 50, 15, 3),
-      new Item('2', 'Amplificador', 5000, 50, 50, 50, 22),
-      new Item('3', 'Cabo', 30, 10, 10, 10, 1),
-    ];
-    this.zipCodeCalculator = new ZipCodeCalculatorMemory();
-  }
-
-  execute(input: PlaceOrderInput): PlaceOrderOutput {
+  async execute(input: PlaceOrderInput): Promise<PlaceOrderOutput> {
     const order = new Order(input.cpf);
-    const distance = this.zipCodeCalculator.calculate(
+    const distance = this.zipcodeCalculator.calculate(
       input.zipcode,
-      '11-111-00'
+      '99.999-99'
     );
     // eslint-disable-next-line no-restricted-syntax
     for (const orderItem of input.items) {
-      const item = this.items.find(i => i.id === orderItem.id);
+      // eslint-disable-next-line no-await-in-loop
+      const item = await this.itemRepository.getById(orderItem.id);
       if (!item) throw new Error('Item not found');
       order.addItem(orderItem.id, item.price, orderItem.quantity);
       order.freight +=
         FreightCalculator.calculate(item, distance) * orderItem.quantity;
     }
-
     if (input.coupon) {
-      const couponFound = this.coupons.find(c => c.code === input.coupon);
-      if (couponFound) order.addCoupon(couponFound);
+      const coupon = this.couponRepository.getByCode(input.coupon);
+      if (coupon) order.addCoupon(coupon);
     }
     const total = order.getTotal();
-    this.orders.push(order);
-    return new PlaceOrderOutput({ total, freight: order.freight });
+    this.orderRepository.save(order);
+    return new PlaceOrderOutput({
+      freight: order.freight,
+      total,
+    });
   }
 }
