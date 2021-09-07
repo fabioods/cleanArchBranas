f3ccab4e9c1fc747b4677b3575184ad6ce424012
Fábio dos Santos <fah_ds@live.com>
2021-09-07 16:59:04 -0300

    refactor: alterado schema utilizado para consulta no item

--- test/integration/PgPromiseDatabase.test.ts
+++ test/integration/PgPromiseDatabase.test.ts
@@ -8,13 +8,13 @@ describe('Pg Promise connect', () => {
   });
 
   it('Deve conectar no banco de dados e listar os 3 itens existentes', async () => {
-    const items = await db.many('SELECT * FROM cleanarch.item', []);
+    const items = await db.many('SELECT * FROM ccca.item', []);
     expect(items).toHaveLength(3);
   });
 
   it('Deve conectar no banco de dados e listar um item existente', async () => {
     const item = await db.one(
-      'SELECT * FROM cleanarch.item where description = $1',
+      'SELECT * FROM ccca.item where description = $1',
       ['Guitarra']
     );
     expect(item.description).toBe('Guitarra');
