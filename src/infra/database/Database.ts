5610e06eb2eea27d9efe561c612f33c8c2885c89
Fábio dos Santos <fah_ds@live.com>
2021-09-07 16:54:22 -0300

    feat: adicionado funcao none para executar delete

--- src/infra/database/Database.ts
+++ src/infra/database/Database.ts
@@ -1,4 +1,5 @@
 export default interface Database {
   many(query: string, parameters: any): Promise<any>;
   one(query: string, parameters: any): Promise<any>;
+  none(query: string, parameters: any): Promise<void>;
 }
