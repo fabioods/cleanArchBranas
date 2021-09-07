541fe7c53b229e85db740577ff3e9c14e6bb1708
Fábio dos Santos <fah_ds@live.com>
2021-09-07 16:52:22 -0300

    feat: adicionado issueDate e sequence a entidade order

--- src/domain/entity/Order.ts
+++ src/domain/entity/Order.ts
@@ -14,15 +14,21 @@ export class Order {
 
   code: OrderCode;
 
+  issueDate: Date;
+
+  sequence: number;
+
   constructor(cpf: string, issueDate = new Date(), sequence = 1) {
     this.cpf = new CPF(cpf);
     this.items = [];
     this.freight = 0;
+    this.issueDate = issueDate;
+    this.sequence = sequence;
     this.code = new OrderCode(issueDate, sequence);
   }
 
   addItem(id: string, price: number, quantity: number): void {
-    this.items.push(new OrderItem(id, price, quantity));
+    this.items.push(new OrderItem(id, quantity, price));
   }
 
   getTotal(): number {
